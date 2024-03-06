0e402636a8f2f50de2e3d343bed993be2424ad11
Tam Duong <duongchauthanhtam@yahoo.com>
2024-03-06 09:10:18 +0700

    Init

---
+++ node_modules/conghaiso/cong.js
@@ -0,0 +1,7 @@
+/* Dependencies */
+
+const areObjects = require('are-objects');
+const areArrays = require('are-arrays');
+
+const sum = (a, b) => a + b
+exports.sum = sum
---
+++ node_modules/truhaiso/tru.js
@@ -0,0 +1,7 @@
+/* Dependencies */
+
+const areObjects = require('are-objects');
+const areArrays = require('are-arrays');
+
+const sub = (a, b) => a - b
+exports.sub = sub
